2451285e1b48514cb3be7eba41c81869c01fe508
catsigma <im@catsigma.com>
2019-12-12 21:33:23 +0000

    feature(emu): fix some instrs

--- src/emu/instr.js
+++ src/emu/instr.js
@@ -6,6 +6,7 @@ import { Stack } from './contract'
 import { readType, createElementByType } from './micheline'
 
 const json_clone = x => x === undefined ? x : JSON.parse(JSON.stringify(x))
+const get_t_lst = t => json_clone(t instanceof Array ? t : [t])
 
 export const instrs = {
   STOP(contract : Contract, stack : Stack, instr : Object) {
@@ -95,17 +96,17 @@ export const instrs = {
   },
   XOR(contract : Contract, stack : Stack, instr : Object) {
     const [a, b] = stack.drop(2)
-    stack.insert(contract.newElement([a.t[0]], instr.annots, instr.prim, null, [a, b]))
+    stack.insert(contract.newElement(get_t_lst(a.t[0]), instr.annots, instr.prim, null, [a, b]))
     return stack
   },
   AND(contract : Contract, stack : Stack, instr : Object) {
     const [a, b] = stack.drop(2)
-    stack.insert(contract.newElement([b.t[0]], instr.annots, instr.prim, null, [a, b]))
+    stack.insert(contract.newElement(get_t_lst(b.t[0]), instr.annots, instr.prim, null, [a, b]))
     return stack
   },
   OR(contract : Contract, stack : Stack, instr : Object) {
     const [a, b] = stack.drop(2)
-    stack.insert(contract.newElement([a.t[0]], instr.annots, instr.prim, null, [a, b]))
+    stack.insert(contract.newElement(get_t_lst(a.t[0]), instr.annots, instr.prim, null, [a, b]))
     return stack
   },
   CONS(contract : Contract, stack : Stack, instr : Object) {
@@ -133,244 +134,124 @@ export const instrs = {
     let stack2 = stack.clone()
 
     stack1.insert(lst)
-    stack1.insert(contract.newElement([lst.t[1]], [], 'HEAD', null, [lst]))
-    stack1 = this.walkCode(instr.args[0], stack1)
-    stack2 = this.walkCode(instr.args[1], stack2)
+    stack1.insert(contract.newElement(get_t_lst(lst.t[1]), [], 'ITEM.0', null, [lst]))
 
-    stack.stack = stack1.combine(stack2)
+    stack1 = contract.walkCode(instr.args[0], stack1)
+    stack2 = contract.walkCode(instr.args[1], stack2)
+
+    stack.stack = stack1.combine(stack2, contract, instr.prim, instr.annots)
     return stack
   },
   IF_LEFT(contract : Contract, stack : Stack, instr : Object) {
-    const [condition] = stack.drop(1)
+    const [or_item] = stack.drop(1)
 
-    if (condition.state === 'default') {
-      const clone1 = stack.clone()
-      const clone2 = stack.clone()
-      clone1.pushCond(condition, 'default2left')
-      clone2.pushCond(condition, 'default2right')
+    let stack1 = stack.clone()
+    let stack2 = stack.clone()
 
-      clone1.insert(condition.children[0])
-      clone2.insert(condition.children[1])
-      const stacks1 = contract.walkCode(instr.args[0], [clone1])
-      const stacks2 = contract.walkCode(instr.args[1], [clone2])
-      return stacks1.concat(stacks2)
+    stack1.insert(contract.newElement(get_t_lst(or_item.t[1]), instr.annots, 'OR.LEFT', null, [or_item]))
+    stack2.insert(contract.newElement(get_t_lst(or_item.t[2]), instr.annots, 'OR.RIGHT', null, [or_item]))
 
-    } else if (condition.state === 'left') {
-      stack.pushCond(condition, 'left')
-      stack.insert(condition.children[0])
-      return contract.walkCode(instr.args[0], [stack])
+    stack1 = contract.walkCode(instr.args[0], stack1)
+    stack2 = contract.walkCode(instr.args[1], stack2)
 
-    } else if (condition.state === 'right') {
-      stack.pushCond(condition, 'right')
-      stack.insert(condition.children[1])
-      return contract.walkCode(instr.args[1], [stack])
-
-    } else {
-      debugger
-      throw `Invalid condition.state in IF_LEFT: ${condition.state || ''}`
-    }
+    stack.stack = stack1.combine(stack2, contract, instr.prim, instr.annots)
+    return stack
   },
   IF_NONE(contract : Contract, stack : Stack, instr : Object) {
-    const [condition] = stack.drop(1)
+    const [option_item] = stack.drop(1)
 
-    const clone1 = stack.clone()
-    const clone2 = stack.clone()
+    let stack1 = stack.clone()
+    let stack2 = stack.clone()
 
-    const stack1 = contract.walkCode(instr.args[0], clone1)
-    const stack2 = contract.walkCode(instr.args[1], clone2)
+    stack2.insert(contract.newElement(get_t_lst(option_item.t[1]), instr.annots, 'OPTION.0', null, [option_item]))
 
-    const new_stack = stack.clone()
-    new_stack.stack =  stack1.equal(stack2).map(x)
-   .forEach(result => {
+    stack1 = contract.walkCode(instr.args[0], stack1)
+    stack2 = contract.walkCode(instr.args[1], stack2)
 
-    })
-    
-    if (condition.children.length) {
-      stack.pushCond(condition, 'some')
-      stack.insert(condition.children[0])
-      return contract.walkCode(instr.args[1], [stack])
-
-    } else if (condition.state === 'none') {
-      stack.pushCond(condition, 'none')
-      return contract.walkCode(instr.args[0], [stack])
-
-    } else if (condition.state === 'default') {
-      const clone1 = stack.clone()
-      const clone2 = stack.clone()
-      clone1.pushCond(condition, 'default2none')
-      clone2.pushCond(condition, 'default2some')
-
-      const mocked_elem = contract.mockElements(contract.fallbackType(condition.t[1]), 'generate')
-      clone2.insert(mocked_elem)
-      
-      return stacks1.concat(stacks2)
-      
-    } else  {
-      debugger
-      throw `Invalid condition.state in IF_NONE: ${condition.state || ''}`
-    }
+    stack.stack = stack1.combine(stack2, contract, instr.prim, instr.annots)
+    return stack
   },
   IF(contract : Contract, stack : Stack, instr : Object) {
     const [condition] = stack.drop(1)
     
-    if (condition.is_concrate) {
-      if (condition.value === 'True') {
-        stack.pushCond(condition, 'true')
-        return contract.walkCode(instr.args[0], [stack])
-      } else if (condition.value === 'False') {
-        stack.pushCond(condition, 'false')
-        return contract.walkCode(instr.args[1], [stack])
-      } else {
-        throw `Invalid condition in IF: ${condition.value}`
-      }
-    } else {
-      const clone1 = stack.clone()
-      const clone2 = stack.clone()
-      clone1.pushCond(condition, 'default2true')
-      clone2.pushCond(condition, 'default2false')
+    let stack1 = stack.clone()
+    let stack2 = stack.clone()
 
-      const stacks1 = contract.walkCode(instr.args[0], [clone1])
-      const stacks2 = contract.walkCode(instr.args[1], [clone2])
-      return stacks1.concat(stacks2)
-    }
+    stack1 = contract.walkCode(instr.args[0], stack1)
+    stack2 = contract.walkCode(instr.args[1], stack2)
+
+    stack.stack = stack1.combine(stack2, contract, instr.prim, instr.annots)
+    return stack
   },
-  LOOP_LEFT(stack : Stack, instr : Object, call_stack_level : number = 0) {
+  LOOP_LEFT(contract : Contract, stack : Stack, instr : Object) {
+    const [or_item] = stack.drop(1)
+
+    let stack1 = stack.clone()
+    let stack2 = stack.clone()
+
+    stack1.insert(contract.newElement(get_t_lst(or_item.t[2]), instr.annots, 'OR.LEFT.RIGHT', null, [or_item]))
+    stack2.insert(contract.newElement(get_t_lst(or_item.t[2]), instr.annots, 'OR.RIGHT', null, [or_item]))
+
+    stack1 = contract.walkCode(instr.args[0], stack1)
+    stack2 = contract.walkCode(instr.args[1], stack2)
+
+    stack.stack = stack1.combine(stack2, contract, instr.prim, instr.annots)
+    return stack
+  },
+  LOOP(contract : Contract, stack : Stack, instr : Object) {
     const [condition] = stack.drop(1)
 
-    if (call_stack_level === 8)
-      return stack
+    let stack1 = stack.clone()
+    let stack2 = stack.clone()
 
-    if (condition.state === 'left') {
-      stack.pushCond(condition, 'left')
-      stack.insert(condition.children[0])
-      const stacks = contract.walkCode(instr.args[0], [stack])
-      return stacks.reduce((acc, stack) => acc.concat(instrs.LOOP_LEFT.call(this, stack, instr, call_stack_level + 1)), [])
+    stack1 = contract.walkCode(instr.args[0], stack1)
+    stack2 = contract.walkCode(instr.args[1], stack2)
 
-    } else if (condition.state === 'right') {
-      stack.pushCond(condition, 'right')
-      stack.insert(condition.children[1])
-      return stack
-
-    } else {
-      const clone1 = stack.clone()
-      const clone2 = stack.clone()
-      clone1.pushCond(condition, 'default2left')
-      clone2.pushCond(condition, 'default2right')
-
-      clone1.pushCond(condition, 'left')
-      const mocked_elem = contract.mockElements(contract.fallbackType(condition.t[1]), 'generate')
-      mocked_elem.continuation = new Continuation(instr.prim, [mocked_elem.clone(), condition])
-      clone1.insert(mocked_elem)
-
-      clone2.pushCond(condition, 'right')
-
-      const stacks1 = contract.walkCode(instr.args[0], [clone1])
-      stacks1.forEach(stack => stack.replace(x => {
-        if (!x.children[1])
-          debugger
-
-        return x.children[1]
-      }))
-      return stacks1.concat(clone2)
-    }
-
-  },
-  LOOP(stack : Stack, instr : Object, call_stack_level : number = 0) {
-    const [condition] = stack.drop(1)
-
-    if (call_stack_level === 8)
-      return stack
-
-    if (condition.is_concrate) {
-      if (condition.value === 'True') {
-        stack.pushCond(condition, 'true')
-        const stacks = contract.walkCode(instr.args[0], [stack])
-        return stacks.reduce((acc, stack) => acc.concat(instrs.LOOP.call(this, stack, instr, call_stack_level + 1)), [])
-
-      } else if (condition.value === 'False') {
-        stack.pushCond(condition, 'false')
-        return stack
-
-      } else {
-        throw `Invalid condition in LOOP: ${condition.value}`
-      }
-    } else {
-      const clone1 = stack.clone()
-      const clone2 = stack.clone()
-      clone1.pushCond(condition, 'default2true')
-      clone2.pushCond(condition, 'default2false')
-
-      const stacks1 = contract.walkCode(instr.args[0], [clone1])
-      stacks1.forEach(stack => {
-        if (!stack.is_failed())
-          stack.drop(1)
-      })
-      return stacks1.concat(clone2)
-    }
+    stack.stack = stack1.combine(stack2, contract, instr.prim, instr.annots)
+    return stack
   },
   SOME(contract : Contract, stack : Stack, instr : Object) {
-    stack.replace(x => contract.newElement({
-      t: ['option', x.t],
-      children: [x],
-      annots: instr.annots,
-      state: 'some'
-    }, 'generate'))
+    stack.replace(x => contract.newElement(
+      ['option', json_clone(x.t)],
+      instr.annots,
+      instr.prim,
+      null,
+      [x]
+    ))
+
     return stack
   },
   NONE(contract : Contract, stack : Stack, instr : Object) {
-    stack.insert(contract.newElement({
-      t: ['option', contract.readType(instr.args[0])],
-      children: [],
-      annots: instr.annots,
-      value: 'NONE',
-      state: 'none'
-    }))
+    stack.replace(x => contract.newElement(
+      ['option', json_clone(x.t)],
+      instr.annots,
+      instr.prim,
+      null,
+      [x]
+    ))
+
     return stack
   },
   ITER(contract : Contract, stack : Stack, instr : Object) {
-    const [item] = stack.drop(1)
-    let stacks = [stack]
-    if (item.children.length) {
-      item.children.forEach(child => {
-        stacks.forEach(stack => stack.insert(child))
-        stacks = contract.walkCode(instr.args[0], stacks)
-      })
-    } else {
-      item.instr = instr
-      item.continuation = new Continuation(instr.prim, [item.clone()])
-    }
-    return stacks
+    const [lst_item] = stack.drop(1)
+
+    let stack1 = stack.clone()
+    let stack2 = stack.clone()
+    
+    stack1.insert(contract.newElement(get_t_lst(lst_item.t[1]), instr.annots, 'ITEM.0', null, [lst_item]))
+    stack1 = contract.walkCode(instr.args[0], stack1)
+
+    stack.stack = stack1.combine(stack2, contract, instr.prim, instr.annots)
+    return stack
   },
   MAP(contract : Contract, stack : Stack, instr : Object) {
-    const [item] = stack.drop(1)
-    const prev_len = stack.length()
-    const prev_dip_top = stack.dip_top
+    const [iter_item] = stack.drop(1)
 
-    let stacks = [stack]
-    if (item.children.length) {
-      item.children.forEach(child => {
-        stacks.forEach(stack => stack.insert(child))
-        stacks = contract.walkCode(instr.args[0], stacks)
-        stacks.forEach(stack => stack.dip_top += 1)
-      })
-      stacks.forEach(stack => {
-        const cloned_item = item.clone()
-        stack.dip_top = prev_dip_top
-        cloned_item.children = stack.drop(stack.length() - prev_len)
-        stack.insert(cloned_item)
-      })
-    } else {
-      item.continuation = new Continuation(instr.prim, [item.clone()])
-      stack.insert(item)
-    }
-    return stacks
+    stack.insert(contract.newElement(get_t_lst(iter_item.t), instr.annots, 'ITEMS', null, [iter_item]))
+    return stack
   },
   FAILWITH(contract : Contract, stack : Stack, instr : Object) {
-    stack.stack.splice(0, 0, contract.newElement({
-      t: ['fail'],
-      annots: instr.annots,
-      continuation: new Continuation(instr.prim, stack.drop(1))
-    }))
+    stack.insert(contract.newElement(['fail'], instr.annots, '', null, stack.drop(1)))
     return stack
   },
   DUP(contract : Contract, stack : Stack, instr : Object) {
@@ -381,9 +262,9 @@ export const instrs = {
     const level = instr.args[0].int ? parseInt(instr.args.shift().int) : 1
     const prev_dip_top = stack.dip_top
     stack.dip_top += level
-    const stacks = contract.walkCode(instr.args[0], [stack])
-    stacks.forEach(stack => stack.dip_top = prev_dip_top)
-    return stacks
+    stack = contract.walkCode(instr.args[0], stack)
+    stack.dip_top = prev_dip_top
+    return stack
   },
   DIG(contract : Contract, stack : Stack, instr : Object) {
     const nth = parseInt(instr.args[0].int)
@@ -397,11 +278,7 @@ export const instrs = {
     return stack
   },
   NOW(contract : Contract, stack : Stack, instr : Object) {
-    stack.insert(contract.newElement({
-      t: ['timestamp'],
-      annots: instr.annots,
-      value: 'NOW'
-    }))
+    stack.insert(contract.newElement(['timestamp'], instr.annots, '', 'NOW', []))
     return stack
   },
   COMPARE(contract : Contract, stack : Stack, instr : Object) {
